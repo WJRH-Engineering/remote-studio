0874896968b52403573c3c7354fb185da9ef3e96
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2020-08-23 12:13:27 -0400

    generate script now pulls data from SQL tables and generates a single JSON file to be read by liquidsoap

--- generate.py
+++ generate.py
@@ -6,98 +6,80 @@ import sys
 import random
 import string
 
-# clear files
-mounts = open('icecast/mounts.xml', 'w')
-mounts.write('')
-mounts.close()
-mounts = open('icecast/mounts.xml', 'a')
+import psycopg2 as pg
 
-sources = open('liq/sources.liq', 'w')
-sources.write('')
-sources.close()
-sources = open('liq/sources.liq', 'a')
+conn = pg.connect("dbname = 'testdb' host = 'api.wjrh.org'")
+cursor = conn.cursor()
 
-switch = open('liq/switch.liq', 'w')
-switch.write('active = switch([\n')
-switch.close()
-switch = open('liq/switch.liq', 'a')
-
-mount_template = '''\
-<mount>
-	<mount-name>/{}</mount-name>
-	<username>source</username>
-	<password>{}</password>
-	<max-listeners>10</max-listeners>
-</mount>
-'''
-
-source_template = '''\
-{} = strip_blank(
-	max_blank=30.,
-	input.http("http://remote.wjrh.org:8000/{}")
-)
-'''
-
+# Helper functions
 def generate_password():
-		chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
-		size = 8
-		return ''.join(random.choice(chars) for x in range(size, 20))
+	chars = string.ascii_uppercase 
+	size = 5
+	return ''.join(random.choice(chars) for x in range(0, 5))
 
+def generate_time_expression(sql_time_range):
+	return '{}w{}h{}m{}s-{}w{}h{}m{}s'.format(
+		sql_time_range.lower.day,
+		sql_time_range.lower.hour,
+		sql_time_range.lower.minute,
+		sql_time_range.lower.second,
+		sql_time_range.upper.day,
+		sql_time_range.upper.hour,
+		sql_time_range.upper.minute,
+		sql_time_range.upper.second
+	)
 
-# Read existing password combinations from file
-password_file = csv.reader(open("passwords.csv", "r+"))
-passwords = {}
-for mountpoint, password in password_file:
-	passwords[mountpoint] = password
+# Update mounts table to include all unique program shortname
+cursor.execute("""
+INSERT INTO mounts (shortname)
+SELECT DISTINCT shortname FROM schedule
+ON CONFLICT DO NOTHING;
+""")
+conn.commit()
+
+# Generate passwords and mountpoint urls for mount entries without them
+cursor.execute("""
+SELECT shortname, password, mountpoint
+FROM mounts;
+""")
+
+rows = cursor.fetchall()
+mounts = []
+for shortname, password, mountpoint in rows:
+	new_shortname = shortname
+	new_password = password or generate_password()
+	new_mountpoint = mountpoint or shortname
+	mounts.append((new_shortname, new_password, new_mountpoint))
+
+for shortname, password, mountpoint in mounts:
+	cursor.execute("""
+	UPDATE mounts
+	SET password = %s, mountpoint = %s
+	WHERE shortname = %s;
+	""", (password, mountpoint, shortname))
 	
+conn.commit()
 
-new_passwords = []
+# clear json mounts file
+mountfile = open('liq/mounts.json', 'w')
+mountfile.write('')
+mountfile.close()
+mountfile = open('liq/mounts.json', 'a')
 
-# get the password for the given mount point
-# from the passwords.csv file. If none exists,
-# generate a new one and append it to the file
-def get_password(name):
-	try:
-		return passwords[name]
-	except:
-		new_password = generate_password()
-		new_passwords.append((name, new_password))
-		return new_password
+# Join mounts and schedule table to get data needed for 
+# JSON file generation
+cursor.execute("""
+SELECT schedule.shortname, time_range, password, mountpoint
+FROM schedule
+LEFT JOIN mounts
+ON schedule.shortname = mounts.shortname;
+""")
+rows = cursor.fetchall()
 
-with open('data.csv', newline='') as csvfile:
-	parser = csv.reader(csvfile, delimiter=',')
-	next(parser) # discard the first row
-	for row in parser:
-		name, author, start_time, end_time = row
+mountfile.write('[\n')
+mount_template = '{{"mount": "{}", "password": "{}", "time": "{}"}},\n'
+for name, time, password, mountpoint in rows:
+	mountfile.write(mount_template.format(mountpoint, password, generate_time_expression(time)))
 
-		# Generate a password for this mountpoint
-		password = get_password(name)
-
-		# Generate Icecast mountpoint
-		mount_string = mount_template.format(name, password)
-		mounts.write(mount_string)
-
-		safename = name.replace('-','_')
-	
-		# Generate Liquidsoap source
-		source_string = source_template.format(safename, name)
-		sources.write(source_string)
-
-		# Generate row in Liquidsoap switch statement
-		switch_string = '\t({{ {}-{} }}, {}),\n'.format(start_time, end_time, safename)
-		switch.write(switch_string)
-
-
-switch.write('])')
-
-# Add new passwords to file
-# password_file.close()
-password_file = open('passwords.csv', 'a')
-for name, password in new_passwords:
-	password_file.write('{},{}\n'.format(name, password))
-
-password_file.close()
-
-mounts.close()
-sources.close()
-switch.close()
+mountfile.write(']')
+mountfile.close()
