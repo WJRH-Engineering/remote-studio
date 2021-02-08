f3425fa50f8f04cbcc20eb53fe8877f1124a300a
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2021-02-08 13:54:49 -0500

    linked dependencies for tools folder

--- tools/schedule.py
+++ tools/schedule.py
@@ -6,7 +6,10 @@ import sys
 output_path = sys.argv[1]
 output = open(output_path, "w")
 
-for timeslot in sql.select("schedule"):
+year = "2020"
+season = "FALL"
+
+for timeslot in sql.select("schedule", [year, season]):
 	output.write(f'{timeslot["shortname"]} @ {sql.timestring(timeslot["time_range"])}\n')
 
 output.close()
